04d9619d9dddee5c3ac12180f61a6e9c1ab66f8f
cuong.duc.bui@advesa.com <cuong.duc.bui@advesa.com>
2024-10-22 14:44:40 +0700

    Make a PDF for personal use at this time only

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 function App() {
 	// Tạo state để lưu trữ giá trị của hai input
 	const [dates, setDates] = useState({
-		dateOfBirth: "",
-		dateOfDie: "",
+		dateOfBirth: "1989-05-30",
+		dateOfDie: "2062-05-30",
 	});
 
 	// Hàm handleChange để cập nhật state khi giá trị của input thay đổi
@@ -92,144 +92,154 @@ function App() {
 	const remainingDifferences = dates.dateOfDie
 		? calculateRemainingDifferences(dates.dateOfDie)
 		: null;
-
-	return (
-		<div className="container w-full text-center mx-auto p-4 flex items-center justify-center h-screen">
-			<div>
-				<h2 className="text-4xl font-bold max-w-[992px]">
-					Commodo aute eu adipisicing consequat adipisicing aliquip duis adipisicing aliquip duis.
-				</h2>
-				<div className="grid grid-cols-2 gap-4 mt-16 max-w-[480px] mx-auto">
-					<div>
-						<label className="block" htmlFor="dateOfBirth">Date of birth</label>
-						<input
-							type="date"
-							id="dateOfBirth"
-							value={dates.dateOfBirth}
-							onChange={handleChange}
-							className="border p-2 m-2"
-						/>
-					</div>
-					<div>
-            <label className="block" htmlFor="dateOfDie">Date of die</label>
-						<input
-							type="date"
-							id="dateOfDie"
-							value={dates.dateOfDie}
-							onChange={handleChange}
-							className="border p-2 m-2"
-						/>
+	const a = 1;
+	if (a === 2)
+		return (
+			<div className="container w-full text-center mx-auto p-4 flex items-center justify-center h-screen">
+				<div>
+					<h2 className="text-4xl font-bold max-w-[992px]">
+						Commodo aute eu adipisicing consequat adipisicing aliquip duis
+						adipisicing aliquip duis.
+					</h2>
+					<div className="grid grid-cols-2 gap-4 mt-16 max-w-[480px] mx-auto">
+						<div>
+							<label className="block" htmlFor="dateOfBirth">
+								Date of birth
+							</label>
+							<input
+								type="date"
+								id="dateOfBirth"
+								value={dates.dateOfBirth}
+								onChange={handleChange}
+								className="border p-2 m-2"
+							/>
+						</div>
+						<div>
+							<label className="block" htmlFor="dateOfDie">
+								Date of die
+							</label>
+							<input
+								type="date"
+								id="dateOfDie"
+								value={dates.dateOfDie}
+								onChange={handleChange}
+								className="border p-2 m-2"
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
+		);
+
+	console.log({ dates });
+
+	return (
+		<div className="h-screen w-full flex items-end">
+			<div className={`${dates.startDate && dates.endDate ? '' : 'hidden'}`}>
+				{remainingDifferences && (
+					<div>
+						<p>Số ngày còn lại: {remainingDifferences.days}</p>
+						<p>Số tuần còn lại: {remainingDifferences.weeks}</p>
+						<p>Số tháng còn lại: {remainingDifferences.months}</p>
+						<p>Số năm còn lại: {remainingDifferences.years}</p>
+						<p>Số lần sinh nhật còn lại: {remainingDifferences.birthdays}</p>
+						<p>Số lần tết holiday còn lại: {remainingDifferences.holidays}</p>
+					</div>
+				)}
+				<div>
+					<label htmlFor="dateOfBirth">Date of birth</label>
+					<input
+						type="date"
+						id="dateOfBirth"
+						value={dates.dateOfBirth}
+						onChange={handleChange}
+						className="border p-2 m-2"
+					/>
+				</div>
+				<div>
+					<label htmlFor="dateOfDie">Date of die</label>
+					<input
+						type="date"
+						id="dateOfDie"
+						value={dates.dateOfDie}
+						onChange={handleChange}
+						className="border p-2 m-2"
+					/>
+				</div>
+			</div>
+			{dateDifferences && (
+				<>
+					<div className="grid grid-cols-7 gap-2 mt-4 hidden">
+						{/* {Array.from({ length: dateDifferences.days }).map((_, index) => {
+							const currentDate = new Date(dates.dateOfBirth);
+							currentDate.setDate(currentDate.getDate() + index);
+							const formattedDate = currentDate.toLocaleDateString();
+							const isPast = currentDate < new Date();
+
+							return (
+								<div
+									key={index}
+									className={`w-32 h-16 border flex items-center justify-center ${
+										isPast ? "bg-gray-200" : "border-gray-300"
+									}`}
+								>
+									{formattedDate}
+								</div>
+							);
+						})} */}
+					</div>
+					<div className="grid grid-cols-56 gap-1 mt-4  mx-auto w-fit ">
+						{Array.from({ length: dateDifferences.weeks }).map((_, index) => {
+							const startDate = new Date(dates.dateOfBirth);
+							startDate.setDate(startDate.getDate() + index * 7);
+							const endDate = new Date(startDate);
+							endDate.setDate(endDate.getDate() + 6);
+							const formattedStartDate = startDate.toLocaleDateString();
+							const formattedEndDate = endDate.toLocaleDateString();
+							const isPast = endDate < new Date();
+
+							// Kiểm tra xem tuần hiện tại có chứa ngày sinh nhật hay không
+							const birthday = new Date(dates.dateOfBirth);
+							birthday.setFullYear(startDate.getFullYear());
+							const isBirthdayWeek =
+								startDate <= birthday && birthday <= endDate;
+
+							// Kiểm tra xem tuần hiện tại có chứa ngày đầu năm mới hay không
+							let isNewYearWeek = false;
+							for (
+								let d = new Date(startDate);
+								d <= endDate;
+								d.setDate(d.getDate() + 1)
+							) {
+								if (d.getMonth() === 0 && d.getDate() === 1) {
+									isNewYearWeek = true;
+									break;
+								}
+							}
+
+							return (
+								<div
+									key={index}
+									className={`w-4 h-4 border flex items-center justify-center ${
+										isPast
+											? "bg-gray-200 hidden"
+											: isBirthdayWeek
+												? ""
+												: isNewYearWeek
+													? "bg-gray-300"
+													: "border-gray-300"
+									}`}
+								>
+									{/* <p className="text-xs">{formattedStartDate} - {formattedEndDate}</p> */}
+									{/* {index + 1} */}
+								</div>
+							);
+						})}
+					</div>
+				</>
+			)}
 		</div>
 	);
-
-	// return (
-	// 	<div className="p-4">
-	// 		{remainingDifferences && (
-	// 			<div>
-	// 				<p>Số ngày còn lại: {remainingDifferences.days}</p>
-	// 				<p>Số tuần còn lại: {remainingDifferences.weeks}</p>
-	// 				<p>Số tháng còn lại: {remainingDifferences.months}</p>
-	// 				<p>Số năm còn lại: {remainingDifferences.years}</p>
-	// 				<p>Số lần sinh nhật còn lại: {remainingDifferences.birthdays}</p>
-	// 				<p>Số lần tết holiday còn lại: {remainingDifferences.holidays}</p>
-	// 			</div>
-	// 		)}
-	// 		<div>
-	// 			<label htmlFor="dateOfBirth">Date of birth</label>
-	// 			<input
-	// 				type="date"
-	// 				id="dateOfBirth"
-	// 				value={dates.dateOfBirth}
-	// 				onChange={handleChange}
-	// 				className="border p-2 m-2"
-	// 			/>
-	// 		</div>
-	// 		<div>
-	// 			<label htmlFor="dateOfDie">Date of die</label>
-	// 			<input
-	// 				type="date"
-	// 				id="dateOfDie"
-	// 				value={dates.dateOfDie}
-	// 				onChange={handleChange}
-	// 				className="border p-2 m-2"
-	// 			/>
-	// 		</div>
-	// 		{dateDifferences && (
-	// 			<>
-	// 				<div className="grid grid-cols-7 gap-2 mt-4 hidden">
-	// 					{Array.from({ length: dateDifferences.days }).map((_, index) => {
-	// 						const currentDate = new Date(dates.dateOfBirth);
-	// 						currentDate.setDate(currentDate.getDate() + index);
-	// 						const formattedDate = currentDate.toLocaleDateString();
-	// 						const isPast = currentDate < new Date();
-
-	// 						return (
-	// 							<div
-	// 								key={index}
-	// 								className={`w-32 h-16 border flex items-center justify-center ${
-	// 									isPast ? "bg-gray-200" : "border-gray-300"
-	// 								}`}
-	// 							>
-	// 								{formattedDate}
-	// 							</div>
-	// 						);
-	// 					})}
-	// 				</div>
-	// 				<div className="grid grid-cols-7 gap-2 mt-4">
-	// 					{Array.from({ length: dateDifferences.weeks }).map((_, index) => {
-	// 						const startDate = new Date(dates.dateOfBirth);
-	// 						startDate.setDate(startDate.getDate() + index * 7);
-	// 						const endDate = new Date(startDate);
-	// 						endDate.setDate(endDate.getDate() + 6);
-	// 						const formattedStartDate = startDate.toLocaleDateString();
-	// 						const formattedEndDate = endDate.toLocaleDateString();
-	// 						const isPast = endDate < new Date();
-
-	// 						// Kiểm tra xem tuần hiện tại có chứa ngày sinh nhật hay không
-	// 						const birthday = new Date(dates.dateOfBirth);
-	// 						birthday.setFullYear(startDate.getFullYear());
-	// 						const isBirthdayWeek =
-	// 							startDate <= birthday && birthday <= endDate;
-
-	// 						// Kiểm tra xem tuần hiện tại có chứa ngày đầu năm mới hay không
-	// 						let isNewYearWeek = false;
-	// 						for (
-	// 							let d = new Date(startDate);
-	// 							d <= endDate;
-	// 							d.setDate(d.getDate() + 1)
-	// 						) {
-	// 							if (d.getMonth() === 0 && d.getDate() === 1) {
-	// 								isNewYearWeek = true;
-	// 								break;
-	// 							}
-	// 						}
-
-	// 						return (
-	// 							<div
-	// 								key={index}
-	// 								className={`w-32 h-16 border flex items-center justify-center ${
-	// 									isPast
-	// 										? "bg-gray-200"
-	// 										: isBirthdayWeek
-	// 											? "bg-yellow-200"
-	// 											: isNewYearWeek
-	// 												? "bg-green-200"
-	// 												: "border-gray-300"
-	// 								}`}
-	// 							>
-	// 								{/* <p className="text-xs">{formattedStartDate} - {formattedEndDate}</p> */}
-	// 								{index + 1}
-	// 							</div>
-	// 						);
-	// 					})}
-	// 				</div>
-	// 			</>
-	// 		)}
-	// 	</div>
-	// );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				24: "repeat(24, minmax(0, 1fr))",
+        48: "repeat(48, minmax(0, 1fr))",
+        56: "repeat(56, minmax(0, 1fr))",
+			},
+		},
+	},
+	plugins: [],
+};
