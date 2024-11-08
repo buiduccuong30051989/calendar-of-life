f558812c9f62d387b07befbbd6068b3c1c2d8487
cuong.duc.bui@advesa.com <cuong.duc.bui@advesa.com>
2024-11-08 11:48:02 +0700

    Public to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "calendar-of-life",
 	plugins: [react(), svgr()],
 	resolve: {
     alias: {
